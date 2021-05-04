8536654444c076531f2d6183ff84deb04b38d7e4
andreltcarvalho <andreltcarvalho@hotmail.com>
2021-05-03 22:33:56 -0300

    projeto criado

---
+++ backend/src/main/java/com/fast/fastguiche/FastguicheApplication.java
@@ -0,0 +1,13 @@
+package com.fast.fastguiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastguicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastguicheApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fast/fastguiche/FastguicheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.fastguiche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastguicheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
