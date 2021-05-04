9eab6cce384f5f104b0c8c178881dca4b5580310
andreltcarvalho <andreltcarvalho@hotmail.com>
2021-05-03 23:20:32 -0300

    Footer e DataTable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div className="App">
-     <h1 className="text-primary">Ola mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="App">
+        <h1 className="text-primary">Ola mundo!</h1>
+      <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
